5450a9bedd88ad66254ecf706c9ddcc8a28e268e
Ian Sillitoe <i.sillitoe@ucl.ac.uk>
2025-02-20 09:25:41 +0000

    move script

--- docker/script/combine_results.py
+++ docker/script/combine_results.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 MERIZO_COLS = 'af_chain_id nres nres_dom nres_ndr ndom pIoU runtime result'.split()
 UNIDOC_COLS = 'af_chain_id result'.split()
-CHAINSAW_COLS = 'af_chain_id sequence_md5	nres ndom	result uncertainty'.split()
+CHAINSAW_COLS = 'af_chain_id sequence_md5	nres ndom	result uncertainty time_sec'.split()
 OUTPUT_COLS = {
     'af_chain_id': 'AlphaFold_ID',
     'result_chainsaw': 'Chainsaw',
@@ -21,30 +21,38 @@ OUTPUT_COLS = {
 @click.option('--output', '-o', 'output_file', 
               required=True, help='Output result file path (TSV)')
 def run(merizo_file, unidoc_file, chainsaw_file, output_file):
+
+    # read merizo results
     merizo_df = pd.read_csv(merizo_file, sep='\t', header=None, names=MERIZO_COLS)
     merizo_df = normalise_df(merizo_df)
     merizo_df.rename(columns={'result': 'result_merizo'}, inplace=True)
     
+    # read unidoc results
     unidoc_df = pd.read_csv(unidoc_file, sep='\t', header=None, names=UNIDOC_COLS)
     unidoc_df = normalise_df(unidoc_df)
     unidoc_df.rename(columns={'result': 'result_unidoc'}, inplace=True)
-    
+
+    # read chainsaw results
+    chainsaw_df = pd.read_csv(chainsaw_file, sep='\t', header=None, names=CHAINSAW_COLS)
+    chainsaw_df = normalise_df(chainsaw_df)
+    chainsaw_df.rename(columns={'result': 'result_chainsaw'}, inplace=True)
+
+    # merge results
     result_df = merizo_df.merge(unidoc_df, on='af_chain_id', how='outer')
+    result_df = result_df.merge(chainsaw_df, on='af_chain_id', how='outer')
 
-    if chainsaw_file:
-        chainsaw_df = pd.read_csv(chainsaw_file, sep='\t', header=None, names=CHAINSAW_COLS)
-        chainsaw_df = normalise_df(chainsaw_df)
-        chainsaw_df.rename(columns={'result': 'result_chainsaw'}, inplace=True)
-        result_df = result_df.merge(chainsaw_df, on='af_chain_id', how='outer')
-    else:
-        result_df['result_chainsaw'] = [None] * result_df.shape[0]
-
+    # format output
     result_df = result_df[OUTPUT_COLS.keys()].rename(columns=OUTPUT_COLS)
     result_df.to_csv(output_file, sep='\t', index=False, header=True)
+    
 
 def normalise_df(df):
     # index by file stem (no suffix)
     df['af_chain_id'] = df['af_chain_id'].str.replace('.pdb', '').str.replace('.cif', '')
+    bad_matches_idx = df['af_chain_id'].str.match(r'(?!AF-)')
+    if bad_matches_idx.any():
+        bad_matches = df.loc[bad_matches_idx, 'af_chain_id'].unique()
+        raise RuntimeError(f"Invalid AlphaFold ID: {bad_matches}")
     return df
 
 
